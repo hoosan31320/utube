b90d60d9b94cf22a34a50a81cacccc93ee2daecd
hoosan31320 <hoos31320@gmail.com>
2021-02-13 04:59:31 +0700

    Express Core Routing

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-}
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -28,4 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.listen(PORT, handleListening);
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
