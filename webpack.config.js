6f4b410006f0d7b8697cf4f77cbb933849ff6b24
hoosan31320 <hoos31320@gmail.com>
2021-02-16 17:28:52 +0700

    Style, ES6 with Webpack---reinstall babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
@@ -6,16 +7,37 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
       rules: [
         {
+          test: /\.(js)$/,
+          use: [
+            {
+              loader: "babel-loader"
+            }
+          ]
+        },
+        {
           test: /\.(scss)$/,
           use: [MiniCssExtractPlugin.loader,
-              'css-loader',
-              'postcss-loader',
-              'sass-loader'
+              "css-loader",
+              {loader:"postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      [
+                        "autoprefixer",
+                        { 
+                          browsers: "cover 99.5%" 
+                        }
+                      ]
+                    ]
+                  }
+                }
+              },
+              "sass-loader"
           ]
         }
       ]
