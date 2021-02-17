335bf19c990df389ddf31a1225264755c2046502
hoosan31320 <hoos31320@gmail.com>
2021-02-18 01:45:28 +0700

    Everything else

--- init.js
+++ init.js
@@ -4,7 +4,7 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000 || process.env.PORT;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
--- routes.js
+++ routes.js
@@ -62,4 +62,4 @@ const routes = {
     }
 };
  
-export default routes
+export default routes;
