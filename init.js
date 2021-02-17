a8ebe2d1af4c7b8ceee9738d4f30d61ac5a37d29
hoosan31320 <hoos31320@gmail.com>
2021-02-18 02:35:42 +0700

    Passport Join and Login

--- init.js
+++ init.js
@@ -1,8 +1,12 @@
-import "./db";
-import "./models/Video";
-import app from "./app";
 import dotenv from "dotenv";
+import "./db";
+import app from "./app";
+
 dotenv.config();
+
+import "./models/Video";
+import "./models/User";
+import "./models/Comment";
 
 const PORT = 4000 || process.env.PORT;
 
