69fd4cf4e7478d5eb673c3d87de74fe20e413bbb
hoosan31320 <hoos31320@gmail.com>
2021-02-21 11:08:34 +0700

    Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,31 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumnBtn = document.getElementById("jsVolumeBtn");
 
 function handlePlayClick() {
     if (videoPlayer.paused) {
         videoPlayer.play();
+        playBtn.innerHTML = ' <i class="fas fa-pause"></i> ';
     } else {
         videoPlayer.pause();
+        playBtn.innerHTML = ' <i class="fas fa-play"></i> ';
+    }
+}
+
+function handleVolumeClick() {
+    if (videoPlayer.muted) {
+        videoPlayer.muted = false;
+        volumnBtn.innerHTML = ' <i class="fas fa-volume-up"></i> '
+    } else {
+        videoPlayer.muted = true;
+        volumnBtn.innerHTML = ' <i class="fas fa-volume-mute"></i> '
     }
 }
 
 function init() {
-    playBtn.addEventListener("click", handlePlayClick);    
+    playBtn.addEventListener("click", handlePlayClick);   
+    volumnBtn.addEventListener("click", handleVolumeClick); 
 }
 
 if (videoContainer) {
