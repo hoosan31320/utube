fef49a1aa71a25922a59060b4795421158ffbc0b
hoosan31320 <hoos31320@gmail.com>
2021-02-22 16:44:56 +0700

    Api Registering a view

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -8,6 +8,15 @@ const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("jsVolume");
 
 
+const registerView = () => {
+    console.log(registerView);
+    const videoId = window.location.href.split("/videos/")[1];
+    fetch( `/api/${videoId}/view`, {
+        method: "POST"
+    });
+};
+
+
 function handlePlayClick() {
     if (videoPlayer.paused) {
         videoPlayer.play();
@@ -90,6 +99,7 @@ function setTotalTime() {
 }
 
 function handleEnded() { 
+    registerView();
     videoPlayer.currentTime = 0;
     playBtn.innerHTML = '<i class="fas fa-play"></i>'; 
 }
